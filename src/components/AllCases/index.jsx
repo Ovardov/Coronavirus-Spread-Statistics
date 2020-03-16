68c3904364832907d6e9643227aecec86079da53
Ovardov <ovardov7@gmail.com>
2020-03-16 21:58:33 +0200

    Changed info box styles

--- src/components/AllCases/index.jsx
+++ src/components/AllCases/index.jsx
@@ -4,15 +4,15 @@ import styles from './all-cases.module.scss'
 const AllCases = ({ cases, recovered, deaths }) => {
   return (
     <section className={styles.container}>
-      <section>
+      <section className={styles.cases}>
         <p>{cases}</p>
         <p className={styles.title}>Confirmed</p>
       </section>
-      <section>
+      <section className={styles.recovered}>
         <p>{recovered}</p>
         <p className={styles.title}>Recovered</p>
       </section>
-      <section>
+      <section className={styles.deaths}>
         <p>{deaths}</p>
         <p className={styles.title}>Deaths</p>
       </section>
--- src/components/Countries/CountryCard/index.jsx
+++ src/components/Countries/CountryCard/index.jsx
@@ -3,15 +3,15 @@ import styles from './country-card.module.scss'
 
 function CountryCard({ cases, recovered, deaths, todayDeaths, todayCases, active, critical }) {
     return (
-        <section className={styles['more-info']}>
-            <p>All Cases: <span className={styles.info}>{cases}</span></p>
-            <p>Recovered: <span className={styles.info}>{recovered}</span></p>
-            <p>Deaths: <span className={styles.info}>{deaths}</span></p>
-            <p>Today Cases: <span className={styles.info}>{todayCases}</span></p>
-            <p>Today Deaths: <span className={styles.info}>{todayDeaths}</span></p>
-            <p>Active: <span className={styles.info}>{active}</span></p>
-            <p>Critical: <span className={styles.info}>{critical}</span></p>
-        </section>
+        <ul className={styles['more-info']}>
+            <li>All Cases: <span className={styles.info}>{cases}</span></li>
+            <li>Recovered: <span className={styles.info}>{recovered}</span></li>
+            <li>Deaths: <span className={styles.info}>{deaths}</span></li>
+            <li>Today Cases: <span className={styles.info}>{todayCases}</span></li>
+            <li>Today Deaths: <span className={styles.info}>{todayDeaths}</span></li>
+            <li>Active: <span className={styles.info}>{active}</span></li>
+            <li>Critical: <span className={styles.info}>{critical}</span></li>
+        </ul>
     )
 }
 
