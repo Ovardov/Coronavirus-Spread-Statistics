04a27add7886338277474d8bf2b025a9913ff8a3
Ovardov <ovardov7@gmail.com>
2020-03-16 12:40:04 +0200

    Add not found route

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -15,7 +15,7 @@ function App() {
             <Switch>
               <Route exact path="/" component={HomePage} />
               
-              {/* <Route path="*" component={NotFound} /> */}
+              <Route path="*" component={HomePage} />
             </Switch>
           </main>
         </div>
