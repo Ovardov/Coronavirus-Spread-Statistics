81eef924a90782fae476b100a56d3c885bbcd490
Ovardov <ovardov7@gmail.com>
2020-03-18 22:22:36 +0200

    Add countries page

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -5,7 +5,7 @@ function Search({ findCountry, setSearchedCountry }) {
   return (
     <form className={styles.container}>
       <p>
-        <input type="text" placeholder="Search" name="search" onChange={e => setSearchedCountry(e.target.value)} />
+        <input type="text" placeholder="Search" name="search" onChange={findCountry} />
       </p>
 
       <button className={styles.button} onClick={findCountry}><i className="fas fa-search"></i></button>
---
+++ src/components/Table/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import styles from './table.module.scss'
+
+const renderCountries = filteredCountries => {
+  return filteredCountries.map(country => {
+    return (
+      <tr key={country.country + country.cases} className={styles.grid}>
+        <td className={styles['two-columns']}>{country.country}</td>
+        <td>{country.cases}</td>
+        <td>{country.todayCases}</td>
+        <td>{country.recovered}</td>
+        <td>{country.deaths}</td>
+        <td>{country.todayDeaths}</td>
+        <td>{country.active}</td>
+        <td>{country.critical}</td>
+      </tr>
+    )
+  })
+}
+
+const Table = ({ filteredCountries }) => {
+  return (
+    <table className={styles.container}>
+      <thead className={styles.header}>
+        <tr className={styles.grid}>
+          <th className={styles['two-columns']}>County</th>
+          <th>Total Cases</th>
+          <th>Today Cases</th>
+          <th>Total Recovered</th>
+          <th>Total Deaths</th>
+          <th>Today Deaths</th>
+          <th>Active</th>
+          <th>Critical</th>
+        </tr>
+      </thead>
+
+      <tbody className={styles.body}>{filteredCountries.length > 0 && renderCountries(filteredCountries)}</tbody>
+    </table>
+  )
+}
+
+export default Table
