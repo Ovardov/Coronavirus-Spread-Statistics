23c33cf4ea6db876cd6777c476b5b46c18e5f692
Ovardov <ovardov7@gmail.com>
2020-03-25 23:09:32 +0200

    Add message when do not have country stats

--- src/components/CountryCard/index.jsx
+++ src/components/CountryCard/index.jsx
@@ -6,17 +6,24 @@ function CountryCard({ country, cases, recovered, deaths, todayDeaths, todayCase
     const { translate } = useTranslations();
 
     return (
-        <ul className={styles['more-info']}>
-            <li>{translate('table.country')}: <span className={styles.info}>{country}</span></li>
-            <li>{translate('table.cases')}: <span className={styles.info}>{cases}</span></li>
-            <li>{translate('table.recovered')}: <span className={styles.info}>{recovered}</span></li>
-            <li>{translate('table.deaths')}: <span className={styles.info}>{deaths}</span></li>
-            <li>{translate('table.todayCases')}: <span className={styles.info}>{todayCases}</span></li>
-            <li>{translate('table.todayDeaths')}: <span className={styles.info}>{todayDeaths}</span></li>
-            <li>{translate('table.active')}: <span className={styles.info}>{active}</span></li>
-            <li>{translate('table.critical')}: <span className={styles.info}>{critical}</span></li>
-            <li>{translate('table.casesPerOneMillion')}: <span className={styles.info}>{casesPerOneMillion}</span></li>
-        </ul>
+        <div className={styles.container}>
+            {cases ? (
+                <ul className={styles['more-info']}>
+                    <li>{translate('table.country')}: <span className={styles.info}>{country}</span></li>
+                    <li>{translate('table.cases')}: <span className={styles.info}>{cases}</span></li>
+                    <li>{translate('table.recovered')}: <span className={styles.info}>{recovered}</span></li>
+                    <li>{translate('table.deaths')}: <span className={styles.info}>{deaths}</span></li>
+                    <li>{translate('table.todayCases')}: <span className={styles.info}>{todayCases}</span></li>
+                    <li>{translate('table.todayDeaths')}: <span className={styles.info}>{todayDeaths}</span></li>
+                    <li>{translate('table.active')}: <span className={styles.info}>{active}</span></li>
+                    <li>{translate('table.critical')}: <span className={styles.info}>{critical}</span></li>
+                    <li>{translate('table.casesPerOneMillion')}: <span className={styles.info}>{casesPerOneMillion}</span></li>
+                </ul>
+            ) : (
+                <p className={styles['no-info']}>Няма намерени резултати</p>
+            )}
+        </div>
+
     )
 }
 
--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,44 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import Marker from './Marker'
 
 function Map({ marker, getCountryData }) {
   const [isLoaded, setIsLoaded] = useState(false)
-  const [map, setMap] = useState(null)
-  const [maps, setMaps] = useState(null)
-
-  const handleLoadedMap = (map, maps) => {
-    setIsLoaded(true)
-    setMap(map)
-    setMaps(maps)
-  }
-
-  // useEffect(() => {
-  //   if (map && maps && marker) {
-  //     // const bounds = new maps.LatLngBounds()
-
-  //     // console.log(bounds)
-
-  //     const position = new maps.LatLng(marker.lat, marker.lng)
-  //     // bounds.extend(position)
-
-  //     // console.log(position)
-
-  //     map.fitBounds(bounds)
-  //   }
-  // }, [marker, maps, map])
-
-
 
   return (
-
     <GoogleMapReact
       bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY, v: '3.32' }}
       defaultCenter={{ lat: 30, lng: 15 }}
       defaultZoom={1}
       center={marker ? { lat: marker.lat, lng: marker.lng } : { lat: 30, lng: 15 }}
       zoom={marker ? 5 : 1}
-      onGoogleApiLoaded={({ map, maps }) => handleLoadedMap(map, maps)}
+      onGoogleApiLoaded={({ map, maps }) => setIsLoaded(true)}
       yesIWantToUseGoogleMapApiInternals
       onClick={getCountryData}
       options={{ gestureHandling: 'greedy', }}
