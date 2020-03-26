6538ef4a60b79351eea2792160c30b2e2bd880a3
Ovardov <ovardov7@gmail.com>
2020-03-26 10:00:13 +0200

    Changed marker position

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -10,12 +10,12 @@ function Map({ marker, getCountryData }) {
       bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY, v: '3.32' }}
       defaultCenter={{ lat: 30, lng: 15 }}
       defaultZoom={1}
-      center={marker ? { lat: marker.lat, lng: marker.lng } : { lat: 30, lng: 15 }}
+      center={marker ? { lat: marker.lat - 4, lng: marker.lng } : { lat: 30, lng: 15 }}
       zoom={marker ? 5 : 1}
       onGoogleApiLoaded={({ map, maps }) => setIsLoaded(true)}
       yesIWantToUseGoogleMapApiInternals
       onClick={getCountryData}
-      options={{ gestureHandling: 'greedy', draggableCursor: 'pointer', }}
+      options={{ gestureHandling: 'greedy', draggableCursor: 'pointer', zoomControl: false }}
     >
       {isLoaded && marker && <Marker lat={marker.lat} lng={marker.lng} marker={marker} />}
     </GoogleMapReact>
