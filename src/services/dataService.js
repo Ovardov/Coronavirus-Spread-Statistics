f4e0097e98929fb0832f4bdcaba63fc3e5dd0984
Ovardov <ovardov7@gmail.com>
2020-03-28 02:01:48 +0200

    Add new geocoding api key

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -15,14 +15,14 @@ export const dataService = {
             .catch(err => console.error(err));
     },
     loadCountryFromLocation: ({lat, lng}) => {
-        return fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&language=en&key=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}`, {
+        return fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&language=en&key=${process.env.REACT_APP_GOOGLE_GEOCODING_API_KEY}`, {
             method: 'GET',
         })
             .then(res => res.json())
             .catch(err => console.error(err));
     },
     loadCountryFromName: (countryName) => {
-        return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${countryName}&language=en&key=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}`, {
+        return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${countryName}&language=en&key=${process.env.REACT_APP_GOOGLE_GEOCODING_API_KEY}`, {
             method: 'GET',
         })
             .then(res => res.json())
